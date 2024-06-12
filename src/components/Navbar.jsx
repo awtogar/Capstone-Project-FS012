ff7f0d7c4a1e0c008e576b7ada1285a1794bc92a
garebizzare <awaludintogar@gmail.com>
2024-06-12 20:55:51 +0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import '../styles/Navbar.css';
+import AppLogo from '../assets/Logo.png'
+
+function Navbar() {
+    return (
+        <header className="header">
+            <nav className="header__navbar">
+                <div className="header__container">
+                    <input className="header__checkbox" type="checkbox" name="navCheckBox" id="navCheckBox" />
+                    <div className="header__hamburger">
+                        <span className="header__line header__line--1"></span>
+                        <span className="header__line header__line--2"></span>
+                    </div>
+                    <div className="header__logo">
+                        <img src={AppLogo} alt="App Logo" className="header__logo-image" />
+                        <a href="#" className="header__logo-text">Epic Indonesia</a>
+                    </div>
+                    <ul className="header__menu">
+                        <li className="header__menu-item">
+                            <a href="#" className="header__menu-link">Home</a>
+                        </li>
+                        <li className="header__menu-item">
+                            <a href="#" className="header__menu-link">Gallery</a>
+                        </li>
+                        <li className="header__menu-item">
+                            <a href="#" className="header__menu-link">Contact</a>
+                        </li>
+                        <li className="header__menu-item">
+                            <a href="#" className="header__menu-link">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
